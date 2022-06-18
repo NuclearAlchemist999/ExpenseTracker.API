2ad4070e70b3803a5cb4c597ce0cf7d980c493d9
Rebecca <lobson999@protonmail.com>
2022-06-18 21:02:35 +0200

    Added account registration

---
+++ ExpenseTracker.API/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using ExpenseTracker.API.DTO.Request;
+using ExpenseTracker.API.Services.AccountService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTracker.API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAccountService _accountservice;
+        public AuthController(IAccountService accountservice)
+        {
+            _accountservice = accountservice;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> CreateAccount(CreateAccountRequest request)
+        {
+            var user = await _accountservice.CreateAccount(request);
+            return Ok(user);
+        }
+    }
+}
--- ExpenseTracker.API/Services/AccountService/IAccountService.cs
+++ ExpenseTracker.API/Services/AccountService/IAccountService.cs
@@ -1,6 +1,10 @@
-﻿namespace ExpenseTracker.API.Services.AccountService
+﻿using ExpenseTracker.API.DTO.DtoModels;
+using ExpenseTracker.API.DTO.Request;
+
+namespace ExpenseTracker.API.Services.AccountService
 {
     public interface IAccountService
     {
+        Task<AccountDto> CreateAccount(CreateAccountRequest request);
     }
 }
