9ae8caeb83857c841474de6f6a38b01cf861a764
Rebecca <lobson999@protonmail.com>
2022-07-08 13:24:18 +0200

    Changed required in dto

--- ExpenseTracker.API/DTO/Request/ExpenseRequestDto.cs
+++ ExpenseTracker.API/DTO/Request/ExpenseRequestDto.cs
@@ -5,12 +5,10 @@ namespace ExpenseTracker.API.DTO.Request
     public abstract record ExpenseRequestDto
     {
         [Required(ErrorMessage = "Title is a required field.")]
-        [MaxLength(30, ErrorMessage = "Maximum length for the title is 30 characters.")]
-        [MinLength(2, ErrorMessage = "Minimum length for the title is 2 characters.")]
         public string Title { get; init; }
         [Required(ErrorMessage = "Category is a required field.")]
         public string CategoryId { get; init; }
-        [Range(1, double.MaxValue, ErrorMessage = "Price is required and it can't be lower than 1")]
+        [Required(ErrorMessage = "Price is a required field.")]
         public double Price { get; init; }
         [Required(ErrorMessage = "Create date is a required field.")]
         public string CreatedAt { get; init; }
